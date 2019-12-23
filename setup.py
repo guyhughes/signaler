924992b0cea8784136d8583617137e43ae00e883
ypcrts <32625281+ypcrts@users.noreply.github.com>
2019-12-22 20:46:19 -0500

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="signaler",
     packages=["signaler"],
-    version="0.0.1",
+    version="0.0.2",
     description=("Simple signal handling"),
     install_requires=["six"],
     author="ypcrts",
